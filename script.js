3794420cb7dd214ae47739ce6c59844d50e47027
pelsdev <pelumiajiteru@gmail.com>
2024-03-06 11:32:27 +0100

    Functions

--- script.js
+++ script.js
@@ -1,1 +1,20 @@
 'use strict';
+
+const calcAverage = (a, b, c) => (a + b + c)/3;
+console.log(calcAverage(2,2,2));
+
+const scoreDolphins = calcAverage(44, 23, 71);
+const scoreKoalas = calcAverage(65, 54, 49);
+
+console.log(scoreDolphins, scoreKoalas);
+
+const checkWinner = function(avgDolpins, avgKoalas){
+    if (avgDolpins >= 2 * avgKoalas){
+        console.log(`Dolphins win (${avgDolpins}) vs. (${avgKoalas})`);
+    }else if  (avgKoalas >= 2 * avgDolpins){
+        console.log(`koalas win (${avgKoalas}) vs. (${avgDolpins})`);
+    }else {
+        console.log(`No team wins....`)
+    }
+}
+checkWinner(scoreDolphins, scoreKoalas);
